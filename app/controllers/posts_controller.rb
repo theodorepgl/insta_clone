fcecfca5002e5913f98e8005ea7e95581b6f38fe
theodorepgl <limlimtheo96@gmail.com>
2023-10-04 13:26:38 +0800

    Second

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:update, :edit, :show, :destroy]
 	def index
-		@posts = Post.all
+		@posts = Post.all.order(created_at: :asc)
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = current_user.posts.create(post_params)
+		if @post.save
+			redirect_to post_path(@post)
+		else
+			render :new
+		end
 	end
 
 	def show
@@ -16,10 +24,25 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		if @post.update(post_params)
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
 	end
 
-	# private 
-	# 	def set_post
-	# 		@posts = Post.find(params[:id])
-	# end
+	def destroy
+		@post.destroy
+
+		redirect_to root_path
+	end
+
+	private
+		def set_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit( :caption, photos: [])
+		end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+	has_many_attached :photos
+	belongs_to :user
 end
